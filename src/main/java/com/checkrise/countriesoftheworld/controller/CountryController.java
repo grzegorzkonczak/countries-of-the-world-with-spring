3ba701d28f035d3e99f9457045fa536415be567e
Grzegorz Kończak <grzegorz.konczak@gmail.com>
2016-11-21 23:56:24 +0100

    Controller class added. Controller has two routes - for index view of all countries and for detailed view of one country.

---
+++ src/main/java/com/checkrise/countriesoftheworld/controller/CountryController.java
@@ -0,0 +1,30 @@
+package com.checkrise.countriesoftheworld.controller;
+
+import com.checkrise.countriesoftheworld.data.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Grzegorz Kończak on 21.11.2016.
+ */
+@Controller
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @RequestMapping("/")
+    public String listCountries(ModelMap modelMap){
+        modelMap.put("countries", countryRepository.getAllCountries());
+        return "index";
+    }
+
+    @RequestMapping("/country/{name}")
+    public String country(@PathVariable String name, ModelMap modelMap){
+        modelMap.put("country", countryRepository.getCountryByName(name));
+        return "country";
+    }
+}
