e01c46a7b99d728385056af61867282d20e438d6
Grzegorz Kończak <grzegorz.konczak@gmail.com>
2016-11-21 23:47:47 +0100

    Added repository class. For this simple project all data is stored in memory using java List.

---
+++ src/main/java/com/checkrise/countriesoftheworld/data/CountryRepository.java
@@ -0,0 +1,45 @@
+package com.checkrise.countriesoftheworld.data;
+
+import com.checkrise.countriesoftheworld.model.Country;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Grzegorz Kończak on 21.11.2016.
+ */
+@Component
+public class CountryRepository {
+    private static final List<Country> ALL_COUNTRIES = Arrays.asList(
+            new Country("Ecuador", 16144000, "Quito", Arrays.asList(
+            "Spanish"
+            )),
+            new Country("France", 66736000, "Paris", Arrays.asList(
+            "French"
+            )),
+            new Country("India", 1293057000, "New Delhi", Arrays.asList(
+            "Hindi", "English"
+            )),
+            new Country("Italy", 66674003, "Rome", Arrays.asList(
+            "Italian"
+            )),
+            new Country("Kenya", 45010056, "Nairobi", Arrays.asList(
+            "English", "Kiswahili"
+            ))
+    );
+
+    public List<Country> getAllCountries() {
+        return ALL_COUNTRIES;
+    }
+
+    public Country getCountryByName(String name){
+        for(Country country : ALL_COUNTRIES){
+            if (country.getName().equals(name)){
+                return country;
+            }
+        }
+        return null;
+    }
+
+}
